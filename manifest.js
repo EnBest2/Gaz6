1f2d57ec4fa8ad296b2e30b81b40282f1125c05b
EnBest2 <enbest2@gmail.com>
2025-04-13 12:31:33 +0200

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,20 @@
+{
+  "short_name": "PénzKezelo",
+  "name": "Pénzügyi Kezelő Alkalmazás",
+  "icons": [
+    {
+      "src": "img/icons/icon-192x192.png",
+      "type": "image/png",
+      "sizes": "192x192"
+    },
+    {
+      "src": "img/icons/icon-512x512.png",
+      "type": "image/png",
+      "sizes": "512x512"
+    }
+  ],
+  "start_url": ".",
+  "display": "standalone",
+  "theme_color": "#007bff",
+  "background_color": "#ffffff"
+}
